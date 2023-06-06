697cda8088b2171e9d82cf355721f8739b3258d3
thyer.myren@fivetran.com <thyer.myren@fivetran.com>
2023-06-06 10:20:16 -0700

    adding in sql server

---
+++ dbt_1/models/example/GCP_SqlServer.sql
@@ -0,0 +1,31 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    /*select 1 as id
+    union all
+    select null as id*/
+
+    /*select *, "hello" as hello from `fivetran-wild-west.thyermyrenpostgresrds_public.employees`*/
+
+    select *, "hello" as hello, "world" as world from {{source('thyermyrenpostgresrds_public','employees')}}
+
+)
+
+select *
+from source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
